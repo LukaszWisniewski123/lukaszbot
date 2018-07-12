cd91608b4c7b869c5cd4a404fcdcb5c34251a21a
LukaszWisniewski123 <lukasz9wisniewski5@gmail.com>
2018-07-12 09:54:16 +0200

    remove last add method

--- biblioteka/src/Map.cpp
+++ biblioteka/src/Map.cpp
@@ -1,29 +1,26 @@
 #include"../biblioteka/includes/Map.hpp"
+
 #include<sstream>
+
 using namespace Lukasz;
 
 void Map::getMap(){
     std::string sTempMapSize;
     std::string sTempMap;
     std::getline(std::cin, sTempMapSize);
-    //Map::getMapSize(sTempMapSize);
 
     while(std::getline(std::cin, sTempMap)){
         Map::vMap.push_back(sTempMap);
     }
 }
 
-bool Map::isWall(std::pair<int, int> cords){
-    std::string sTemp = Map::vMap[cords.first];
+bool Map::isWall(std::pair<int, int> coords){
+    std::string sTemp = Map::vMap[coords.first];
 
-    if(sTemp[cords.second] == '#'){
+    if(sTemp[coords.second] == '#'){
         return true;
     }
     else{
         return false;
     }
-}
-
-void Map::getMapSize(std::string &tempMapSize){
-    std::istringstream sStream;   
 }
